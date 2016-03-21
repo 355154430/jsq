abe3737deb31a3ef1a541885356dfab5f5d8c615
355154430 <355154430@qq.com>
2016-03-21 19:55:15 +0800

    1.0
    1.0

---
+++ ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  jsq
+//
+//  Created by King on 16/3/17.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var b2: UITextField!
+    @IBOutlet weak var b1: UITextField!
+    @IBOutlet weak var out: UITextField!
+    @IBAction func add(sender: UIButton) {
+        var a:Double!=0
+        var b:Double!=0
+        var c:Double!=0
+        if(!b1.text!.isEmpty){
+            a=(b1.text! as NSString).doubleValue// 定义X
+        }
+        if(!b2.text!.isEmpty){
+            b=(b2.text! as NSString).doubleValue// 定义Y
+        }
+        c=a+b
+        out.text="\(c)"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.//////
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
