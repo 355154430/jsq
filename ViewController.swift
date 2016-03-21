0451dda8b7b28702add622b9c6fddec18631ce4a
355154430 <355154430@qq.com>
2016-03-21 20:15:51 +0800

    1.3
    1.3

--- ViewController.swift
+++ ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var b1: UITextField!
     @IBOutlet weak var out: UITextField!
     @IBAction func add(sender: UIButton) {
+        //var me.(a,v,b,)
         //b1=1
         var a:Double!=0
         var b:Double!=0
