f873590069ccd1a3d7def951276d8ab02e64ad79
355154430 <355154430@qq.com>
2016-03-24 11:53:29 +0800

    3.3.
    3.3

--- ViewController.swift
+++ ViewController.swift
@@ -16,12 +16,12 @@ class ViewController: UIViewController {
     var operand2: String = ""
     var operatoe: String = ""
     
-    @IBAction func jisuan(sender: UIButton) {
+   /* @IBAction func jisuan(sender: UIButton) {
        // println("\(sender.currentTitle)")
         //println("\(sender.currentTitle)")
         let value = sender.currentTitle
-        if(value == "+" || value == "-" || value == "X" ||value=="/"){
-            operatoe = value
+        if(value=="+"||value=="-"||value=="X"||value=="/") {
+            operatoe = value!
             return
         }
         else if value == "="{
@@ -32,12 +32,27 @@ class ViewController: UIViewController {
             return
         }
         if operatoe==""{
-            operand1 = operand1 +value
+            operand1 = operand1 + value
         }
         else{
-            operand2 = operand2 +value
+            operand2 = operand2 + value
         }
         
+    }
+    */
+    
+    @IBAction func chu(sender: UIButton) {
+        operand1 / operand2
+    }
+    @IBAction func cehng(sender: UIButton) {
+        operand1 * operand2
+    }
+    @IBAction func jian(sender: UIButton) {
+        operand1 - operand2
+    }
+    @IBAction func jia(sender: UIButton) {
+        operand1 + operand2
+         //operand1 - operand2
     }
         //@IBOutlet weak var b1: UIButton!
     //@IBOutlet weak var b2: UITextField!
