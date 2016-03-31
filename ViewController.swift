1bd358999dff1e657b9f2682ccb855fae29c405a
355154430 <355154430@qq.com>
2016-03-31 11:01:53 +0800

    4.4
    4.4

--- ViewController.swift
+++ ViewController.swift
@@ -47,9 +47,13 @@ class ViewController: UIViewController {
     */
     
     
+    @IBAction func AC(sender: UIButton) {
+        operand.removeAtIndex(operand.endIndex.predecessor())
+    }
     
    @IBAction func jiu(sender: UIButton) {
         operand+="9"
+    print("operand=\(operand)")
     }
     @IBAction func ba(sender: UIButton) {
         operand+="8"
@@ -78,69 +82,102 @@ class ViewController: UIViewController {
     @IBAction func ling(sender: UIButton) {
         operand+="0"
     }
-    
+    //Abc.text = "\(operand)"
     @IBAction func chu(sender: UIButton) {
         x=1
-        y=y++
+        y=y+1
         z=1
+        print("y=\(y)")
+        operand1 = ((operand) as NSString).doubleValue
+        print("operand1=\(operand1)")
+        operand = ""
         //Int(operand1)! / Int(operand2)!
     }
     @IBAction func cehng(sender: UIButton) {
         x=2
-        y=y++
+        y=y+1
         z=1
+        print("y=\(y)")
+        operand1 = ((operand) as NSString).doubleValue
+        print("operand1=\(operand1)")
+        operand = ""
         //Int(operand1)! * Int(operand2)!
     }
     @IBAction func jian(sender: UIButton) {
         x=3
-        y=y++
+        y=y+1
         z=1
+        print("y=\(y)")
+        operand1 = ((operand) as NSString).doubleValue
+        print("operand1=\(operand1)")
+        operand = ""
         //Int(operand1)! - Int(operand2)!
     }
     @IBAction func jia(sender: UIButton) {
         x=4
-        y=y++
+        y=y+1
         z=1
-        //Int(operand1)! + Int(operand2)!
-         //operand1 - operand2
+        operand1 = ((operand) as NSString).doubleValue
+        print("y=\(y)")
+        print("x=\(x)")
+        print("operand1=\(operand1)")
+        operand = ""
+
     }
-  
     @IBAction func dengyu(sender: UIButton) {
-       
-        for(y;y>0;y--)
-        {
-            if(z==1) {
-                operand2=((operand3) as NSString).doubleValue
+       print("y=\(y)")
+       print("z=\(z)")
+        //for(y;y>0;y--)
+        //{
+         //   if(z==1) {
+                operand2=((operand) as NSString).doubleValue
+               // print(operand2)
+                print("operand2=\(operand2)")
+                operand = ""
                 //operand2 =((operand3) as NSString).doubleValue
+           // }
+        /*
+           if(z==0) {
+                operand1=((operand) as NSString).doubleValue
+                print(operand1)
+                operand = ""
             }
-            if(z==0) {
-                operand1=((operand3) as NSString).doubleValue
-            }
+            */
             //var  xxx:Double
             //xxx=((operand1) as NSString).doubleValue
             
             if(x==1) {
                 //(Int)(operand1 !)/(Int)(operand2 !)
                 //operand3=((operand1) as NSString).doubleValue/((operand2) as NSString).doubleValue
+                print("operand1=\(operand1)")
                 operand1=operand1 / operand2
+                print("operand1=\(operand1)")
             }
             if(x==2) {
                 //Int(operand1)! * Int(operand2)!
                 //operand3=((operand1) as NSString).doubleValue*((operand2) as NSString).doubleValue
+                print("operand1=\(operand1)")
                 operand1=operand1 * operand2
+                print("operand1=\(operand1)")
             }
             if(x==3) {
                 //Int(operand1)! - Int(operand2)!
                 //operand3=((operand1) as NSString).doubleValue-((operand2) as NSString).doubleValue
+                print("operand1=\(operand1)")
                 operand1=operand1 - operand2
+                print("operand1=\(operand1)")
             }
             if(x==4) {
                // Int(operand1)! + Int(operand2)!
                 //operand3=((operand1) as NSString).doubleValue+((operand2) as NSString).doubleValue
+                print("operand1=\(operand1)")
                 operand1=operand1 + operand2
+                print("operand1=\(operand1)")
             }
-        }
-        //Abc.text= operand1
+        //var yyy: String = String(operand1)!
+        //Abc.text = "\(operand1)"
+        Abc.text = "\(operand1)"
+   //print("operand1=\(operand1)")
     }
    
   
